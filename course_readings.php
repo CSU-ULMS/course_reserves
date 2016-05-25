40d5190b13c957949cd6136c2db3e205fb1bb652
Ian Chan <ichan@csusm.edu>
2016-05-25 12:00:29 -0700

    Improve UI and add pop-open, dynamic-load iframe src for display of holdings.

--- course_readings.php
+++ course_readings.php
@@ -20,60 +20,76 @@ curl_close($ch);
 $xml = new SimpleXMLElement($response);
 
 include("header.php");
-
+echo "<h1 class=\"page-title\">" . $xml->name . "</h1>\n";
 echo "<table class=\"table table-bordered\">\n";
-echo "<tr>\n<th>Course</th>\n<td>\n";
-echo $xml->name . "\n";
-echo "</td>\n<tr>\n";
-echo "<tr>\n<th>Department</th>\n<td>\n";
-echo $xml->academic_department . "\n";
-echo "<tr>\n<th>Term</th>\n<td>\n";
-echo $xml->terms->term[0] . "\n";
 echo "<tr>\n<th>Instructor</th>\n<td>\n";
 echo $xml->instructors->instructor[0]->first_name . " ";
 echo $xml->instructors->instructor[0]->last_name . "\n";
-echo "</td>\n<tr>\n</table>\n";
+echo "</td>\n<tr>\n";
+echo "<tr>\n<th>Department</th>\n<td>\n";
+echo $xml->academic_department . "\n";
+echo "</td>\n<tr>\n";
+echo "<tr>\n<th>Terms</th>\n<td>\n";
+foreach ($xml->terms->children() as $term) {
+  echo ucwords(strtolower($term)) . " / \n";
+} echo "</td>\n<tr>\n";
+echo "<tr>\n<th>Course Details</th>\n<td>\n";
+echo "<div>Status: " . ucfirst(strtolower($xml->status)) . "</div>\n";
+echo "<div>Processing Department: " . $xml->processing_department['desc'] . "</div>\n";
+echo "<div>Start Date: " . $xml->start_date . "</div>\n";
+echo "<div>End Date: " . $xml->end_date . "</div>\n";
+echo "<div>Code: " . $xml->code . "</div>\n";
+echo "<div>Alma ID: " . $xml->id . "</div>\n";
+echo "</td>\n</tr>\n</table>\n";
 
-echo "<div class=\"row\">\n";
-echo "<table class=\"table table-bordered\" id=\"citationsTable\">\n";
-echo "<thead>\n";
-echo "<tr>\n";
-echo "<th>Title</th>\n";
-echo "<th>Author</th>\n";
-echo "<th>Call Number</th>\n";
-echo "<th>Pages</th>\n";
-echo "<th>Public Note</th>\n";
-echo "</tr>\n";
-echo "</thead>\n";
-echo "<tbody>\n";
 foreach ($xml->reading_lists->children() as $reading_list) {
   if ($reading_list->status['desc'] != "Inactive") {
+    echo "<div class=\"row\">\n";
+    echo "<h3>Reading List Information</div>\n";
+    echo "<div>Name: " . $reading_list->name . "</div>\n";
+    echo "<div>Status: " . $reading_list->status['desc'] . "</div>\n";
+    echo "<div>Code: " . $reading_list->code . "</div>\n";
+    echo "<div>Alma ID: " . $reading_list->id . "</div>\n";
+    echo "<table class=\"table table-bordered\" id=\"citationsTable\">\n";
+    echo "<thead>\n";
+    echo "<tr>\n";
+    echo "<th>Title</th>\n";
+    echo "<th>Author</th>\n";
+    echo "<th>Call Number</th>\n";
+    echo "<th>Pages</th>\n";
+    echo "<th>Public Note</th>\n";
+    echo "</tr>\n";
+    echo "</thead>\n";
+    echo "<tbody>\n";
     foreach ($reading_list->citations->citation as $citation) {
       if ($citation->status == "Complete" || $citation->status == "BeingPrepared") {
         echo "<tr>\n";
         if ($citation->type == "BK") {
           $genre = "book";
           $resolver_tab = "getit";
+          $itemtitle = $citation->metadata->title;
         } elseif ($citation->type == "CR" || $citation->type == "E_CR") {
           $genre = "article";
           $resolver_tab = "viewit";
+          $itemtitle = $citation->metadata->article_title;
         }
         echo "<td>\n";
-        echo "<a href=\"" . $citation->open_url . "\">";
+        echo "<div class=\"iteminfo\"><div>Title: <a class=\"getinfo\" href=\"https://na01.alma.exlibrisgroup.com/view/uresolver/01CALS_USM/openurl?ctx_enc=info:ofi/enc:UTF-8&url_ctx_fmt=info:ofi/fmt:kev:mtx:ctx&url_ver=Z39.88-2004&ctx_enc=info:ofi/enc:UTF-&response_type=xml&isSerivcesPage=true&rft.btitle=";
+          echo urlencode($itemtitle) . "&rft.genre=";
+          echo urlencode($genre) . "&rft.mms_id=";
+          echo $citation->metadata->mms_id . "&rft.au=";
+          echo urlencode($citation->metadata->author) . "&rft.title=";
+          echo urlencode($itemtitle) . "&customer=1670&rft_dat=language=eng,view=cals_usm_services_page&svc_dat=";
+          echo $resolver_tab . "&svc.profile=";
+          echo $resolver_tab . "&env_type=test&req.skin=csusm_uresolver\">\n";
+          echo $itemtitle . "</a></div>\n";
         if ($citation->type == "BK") {
-          echo $citation->metadata->title;
-        } elseif ($citation->type == "CR" || $citation->type == "E_CR") {
-          echo $citation->metadata->article_title . "\n";
-        }
-        echo "</a>\n";
-        echo "<iframe src=\"https://na01.alma.exlibrisgroup.com/view/uresolver/01CALS_USM/openurl?ctx_enc=info:ofi/enc:UTF-8&url_ctx_fmt=info:ofi/fmt:kev:mtx:ctx&url_ver=Z39.88-2004&ctx_enc=info:ofi/enc:UTF-&response_type=xml&isSerivcesPage=true&rft.btitle=";
-        echo urlencode($citation->metadata->title) . "&rft.genre=";
-        echo urlencode($genre) . "&rft.mms_id=";
-        echo $citation->metadata->mms_id . "&rft.au=";
-        echo urlencode($citation->metadata->author) . "&rft.title=";
-        echo urlencode($citation->metadata->title) . "&customer=1670&rft_dat=language=eng,view=cals_usm_services_page&svc_dat=";
-        echo $resolver_tab . "&svc.profile=";
-        echo $resolver_tab . "&env_type=test&req.skin=csusm_uresolver\"></iframe>\n";
+          echo "<div>Publisher: " . $citation->metadata->publisher . "</div>\n";
+          echo "<div>Publication Date: " . $citation->metadata->publication_date . "</div>\n";
+        } 
+        echo "<div>Status: " . $citation->status['desc'] . "</div>\n";
+        echo "<div>Citation ID: " . $citation->id . "</div>\n";
+        echo "<iframe src=\"\"></iframe></div>";
         echo "</td>\n";
         echo "<td>\n";
         echo $citation->metadata->author . "\n";
@@ -99,9 +115,9 @@ foreach ($xml->reading_lists->children() as $reading_list) {
         echo "</tr>\n";
       }
     }              
+    echo "</tbody>\n";
+    echo "</table>\n";
+    echo "</div>\n";
   }
 }
-echo "</tbody>\n";
-echo "</table>\n";
-echo "</div>\n";
 include("footer.php");
--- header.php
+++ header.php
@@ -14,8 +14,10 @@ date_default_timezone_set('America/Los_Angeles');
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/r/bs-3.3.5/jq-2.1.4,dt-1.10.8/datatables.min.css"/>
     <script type="text/javascript" src="https://cdn.datatables.net/r/bs-3.3.5/jqc-1.11.3,dt-1.10.8/datatables.min.js"></script>
+    <script type="text/javascript" src="iframe-resizer/src/iframeResizer.js"></script>
     <style type="text/css">
-      iframe {width: 100%; max-width: 900px;min-height: 170px;border: 1px solid #ddd;padding: 0 20px;}
+      iframe {display: none;}
+      iframe.visible {display: block; width: 100%; max-height: 100%; min-height: 250px; border: 1px solid #ddd;padding: 0 20px;}
     </style>
 
     <!-- Latest compiled and minified JavaScript -->
@@ -32,6 +34,12 @@ date_default_timezone_set('America/Los_Angeles');
           "lengthMenu": [[-1, 25, 50], ["All", 25, 50]],
           "dom": '<"top"ifl>rt<"bottom"p>'
         }); 
+        $('iframe.visible').iFrameResize({"checkOrigin": false});
+        $("a.getinfo").click(function(e) {
+          e.preventDefault();
+          $(this).parents('.iteminfo').children('iframe').attr("src", $(this).attr("href"));
+          $(this).parents('.iteminfo').children('iframe').toggleClass("visible");
+        })
       }); 
     </script>
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -42,16 +50,31 @@ date_default_timezone_set('America/Los_Angeles');
     <![endif]-->
   </head>
   <body>
+    <div class="container-fluid">
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Course Reserves</a>
+          </div>
+
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+            <li><a href="http://biblio.csusm.edu/">Library Home Page</a></li>
+            <li><a href="https://na01.alma.exlibrisgroup.com/institution/01CALS_USM">Alma</a></li>
+            <li><a href="http://primo-pmtna01.hosted.exlibrisgroup.com/primo_library/libweb/action/search.do?mode=Basic&vid=CALS_USM&tab=cals_usm_cr&">Reserves</a></li>
+            <li><a href="https://lib.csusm.edu/helpdesk/">Library Technology Support</a></li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+      </nav>
+    </div>
     <div class="container">
-      <div class="header clearfix">
-        <nav>
-          <ul class="nav nav-pills pull-right">
-            <li role="presentation"><a href="http://biblio.csusm.edu/">Library Home Page</a></li>
-            <li role="presentation"><a href="https://na01.alma.exlibrisgroup.com/institution/01CALS_USM">Alma</a></li>
-            <li role="presentation"><a href="http://primo-pmtna01.hosted.exlibrisgroup.com/primo_library/libweb/action/search.do?mode=Basic&vid=CALS_USM&tab=cals_usm_cr&">Reserves</a></li>
-            <li role="presentation"><a href="https://lib.csusm.edu/helpdesk/">Library Technology Support</a></li>
-          </ul>
-        </nav>
-        <h3 class="text-muted">Course Reserves</h3>
-      </div>
       <div class="row">
